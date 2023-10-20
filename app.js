bc21a1691a16645b76c30ee66ca28fb84e844382
Asif-patel <asifp6021@gmail.com>
2023-10-20 17:14:54 +0530

    flip card and matched card

---
+++ app.js
@@ -0,0 +1,38 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let cardIsFlipped = false;
+let firstCard, secondCard;
+
+cards.forEach(function (card) {
+	card.addEventListener('click', flipCard);
+});
+
+function flipCard() {
+	this.classList.toggle('flip');
+
+	if (!cardIsFlipped) {
+		// first click -> first card
+		cardIsFlipped = true;
+		firstCard = this;
+	} else {
+		// second click -> second card
+		cardIsFlipped = false;
+		secondCard = this;
+
+		//checking whether second card matches or not
+		if (firstCard.dataset.name === secondCard.dataset.name) {
+			// if it is matched -> removing event Listener
+
+			firstCard.addEventListener('click', flipCard);
+
+			secondCard.removeListener('click', flipCard);
+		} else {
+			// if not matches -> removing flip
+
+			setTimeout(() => {
+				firstCard.classList.remove('flip');
+				secondCard.classList.remove('flip');
+			}, 1500);
+		}
+	}
+}
