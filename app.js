e37f87d00d036350055d56807df45da63d78df87
Asif-patel <asifp6021@gmail.com>
2023-10-21 09:49:06 +0530

    5 -> added lockBoard functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let lockBoard = false;
 let cardIsFlipped = false;
 let firstCard, secondCard;
 
@@ -8,41 +9,44 @@ cards.forEach(function (card) {
 });
 
 function flipCard() {
+	if (lockBoard) return; //if card matches locked board
+
 	this.classList.add('flip');
 
 	if (!cardIsFlipped) {
 		// first click -> first card
 		cardIsFlipped = true;
 		firstCard = this;
-	} else {
-		// second click -> second card
-		cardIsFlipped = false;
-		secondCard = this;
 
-		checkForMatch();
+		return;
 	}
+	// second click -> second card
+	cardIsFlipped = false;
+	secondCard = this;
+
+	checkForMatch();
 }
 
 function checkForMatch() {
 	//checking whether second card matches or not
-
 	let isMatched = firstCard.dataset.name === secondCard.dataset.name;
-
 	isMatched ? disableCards() : unFlippedCards();
 }
 
 function disableCards() {
 	// if it is matched -> removing event Listener
-
 	firstCard.removeEventListener('click', flipCard);
-
 	secondCard.removeEventListener('click', flipCard);
 }
 
 function unFlippedCards() {
+	lockBoard = true;
+
 	// if not matches -> removing flip
 	setTimeout(() => {
 		firstCard.classList.remove('flip');
 		secondCard.classList.remove('flip');
+
+		lockBoard = false;
 	}, 1500);
 }
